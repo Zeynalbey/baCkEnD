deda91ba201d4ff91405169eac14da93c08e0265
Zeynalbey <zeynaleg@code.edu.az>
2023-01-23 18:11:17 +0400

    Client - home - index created.

--- Backend_FInal/wwwroot/admin/assets/pages/analytics-customers.init.js
+++ Backend_FInal/wwwroot/admin/assets/pages/analytics-customers.init.js
@@ -211,18 +211,18 @@ var chart1 = new ApexCharts(document.querySelector("#line-1"), options1);
 
 //define data array
 var tabledata = [
-  {id:1, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-1.jpg'>", name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red", dob:"19/02/1984", car:1},
-  {id:2, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-2.jpg'>", name:"Mary May", progress:1, gender:"female", rating:2, col:"blue", dob:"14/05/1982", car:true},
-  {id:3, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-3.jpg'>", name:"Christine Lobowski", progress:42, gender:"female", rating:0, col:"green", dob:"22/05/1982", car:"true"},
-  {id:4, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-4.jpg'>", name:"Brendon Philips", progress:100, gender:"male", rating:1, col:"orange", dob:"01/08/1980"},
-  {id:5, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-5.jpg'>", name:"Margret Marmajuke", progress:16, gender:"female", rating:5, col:"yellow", dob:"31/01/1999"},
-  {id:6, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-6.jpg'>", name:"Frank Harbours", progress:38, gender:"male", rating:4, col:"red", dob:"12/05/1966", car:1},
-  {id:1, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-7.jpg'>", name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red", dob:"19/02/1984", car:1},
-  {id:2, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-8.jpg'>", name:"Mary May", progress:1, gender:"female", rating:2, col:"blue", dob:"14/05/1982", car:true},
-  {id:3, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-9.jpg'>", name:"Christine Lobowski", progress:42, gender:"female", rating:0, col:"green", dob:"22/05/1982", car:"true"},
-  {id:4, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-10.jpg'>", name:"Brendon Philips", progress:100, gender:"male", rating:1, col:"orange", dob:"01/08/1980"},
-  {id:5, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-2.jpg'>", name:"Margret Marmajuke", progress:16, gender:"female", rating:5, col:"yellow", dob:"31/01/1999"},
-  {id:6, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='assets/images/users/user-4.jpg'>", name:"Frank Harbours", progress:38, gender:"male", rating:4, col:"red", dob:"12/05/1966", car:1},
+  {id:1, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-1.jpg'>", name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red", dob:"19/02/1984", car:1},
+  {id:2, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-2.jpg'>", name:"Mary May", progress:1, gender:"female", rating:2, col:"blue", dob:"14/05/1982", car:true},
+  {id:3, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-3.jpg'>", name:"Christine Lobowski", progress:42, gender:"female", rating:0, col:"green", dob:"22/05/1982", car:"true"},
+  {id:4, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-4.jpg'>", name:"Brendon Philips", progress:100, gender:"male", rating:1, col:"orange", dob:"01/08/1980"},
+  {id:5, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-5.jpg'>", name:"Margret Marmajuke", progress:16, gender:"female", rating:5, col:"yellow", dob:"31/01/1999"},
+  {id:6, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-6.jpg'>", name:"Frank Harbours", progress:38, gender:"male", rating:4, col:"red", dob:"12/05/1966", car:1},
+  {id:1, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-7.jpg'>", name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red", dob:"19/02/1984", car:1},
+  {id:2, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-8.jpg'>", name:"Mary May", progress:1, gender:"female", rating:2, col:"blue", dob:"14/05/1982", car:true},
+  {id:3, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-9.jpg'>", name:"Christine Lobowski", progress:42, gender:"female", rating:0, col:"green", dob:"22/05/1982", car:"true"},
+  {id:4, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-10.jpg'>", name:"Brendon Philips", progress:100, gender:"male", rating:1, col:"orange", dob:"01/08/1980"},
+  {id:5, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-2.jpg'>", name:"Margret Marmajuke", progress:16, gender:"female", rating:5, col:"yellow", dob:"31/01/1999"},
+  {id:6, image: "<img alt='' style='width: 30px; height: 30px' class='rounded' src='~/client/assets/images/users/user-4.jpg'>", name:"Frank Harbours", progress:38, gender:"male", rating:4, col:"red", dob:"12/05/1966", car:1},
 ];
 
 
